e33c0187572e4b6c33e59851f44a4f4a4b93793d
ashutosh <ashutoshbanergee939@gmail.com>
2026-04-06 20:56:58 +0530

    project code updated

--- 04_password_genrator/src/App.jsx
+++ 04_password_genrator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect ,useCallback} from 'react'
 
 
 
@@ -10,13 +10,13 @@ function App() {
   const[isSpecialCharUsed,setIsSpecialCharUsed]=useState(true)
   const[isUpperCaseUsed,setIsUpperCaseUsed]=useState(true)
 
-  const genretatePassword=()=>{
+  const genretatePassword=useCallback(()=>{
     let str="abcdefghijklmnopqrstuvwxyz"
     let pass=""
 
     if(isNumberUsed)str+="0123456789"
     if(isSpecialCharUsed)str+="!@#$%^&*"
-    if(isUpperCaseUsed)str+"ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀꜱᴛᴜᴠᴡxʏᴢ"
+    if(isUpperCaseUsed)str+="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
     for(let i=0;i<lenght;i++){
       let random=Math.floor(Math.random()*str.length)
@@ -24,14 +24,37 @@ function App() {
     }
     setPassword(pass)
     
-  }
-  genretatePassword()
-  console.log(password);
+  })
   
+  useEffect(()=>{
+    genretatePassword()
+  },[lenght,isNumberUsed,isSpecialCharUsed,isUpperCaseUsed])
+
+  const copyPassowrd=()=>{
+    window.navigator.clipboard.writeText(password)
+    alert("password copied!!")
+  }
 
   return (
     <>
-      
+      <h1>Password Generator</h1>
+      <input
+      value={password}
+      readOnly
+      >
+      </input>
+      <input type="number"
+      value={lenght}
+      onChange={(e)=>setLenght(Number(e.target.value))}
+      />
+      <button onClick={copyPassowrd}>Copy</button>
+      <p>Numbers</p>
+      <input type="checkbox" checked={isNumberUsed} onChange={()=>setIsnumberUsed(!isNumberUsed)} />
+      <p>Uppercase</p>
+      <input type="checkbox" checked={isUpperCaseUsed} onChange={()=>setIsUpperCaseUsed(!isUpperCaseUsed)} />
+      <p>Charaters</p>
+      <input type="checkbox" checked={isSpecialCharUsed} onChange={()=>setIsSpecialCharUsed(!isSpecialCharUsed)} />
+      <button onClick={genretatePassword}>Generate</button>
     </>
   )
 }
